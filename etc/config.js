e426f160faec245a8db01def58e100ec9af280e7
Vlad1slav <svlad1999@ukr.net>
2020-08-01 00:33:58 +0300

    Upd: basic folders structure (fixes #1)

--- etc/config.js
+++ etc/config.js
@@ -1,7 +1,11 @@
-const appPort = process.env.PORT || 3000;
+import dotenv           from 'dotenv';
+
+dotenv.config();
+
+const appPort = process.env.appPort || 3000;
 const dbConfig = {
     database : process.env.DB || 'base',
-    username : process.env.USERNAME || 'base',
+    username : process.env.DBUSERNAME || 'base',
     password : process.env.PASS || 'base',
     host     : process.env.HOST || '127.0.0.1',
     port     : process.env.DBPORT || 3306,
