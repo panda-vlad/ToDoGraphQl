3625292f92cdbb40256cd3f7e669c0bbc75227cb
Vlad1slav <svlad1999@ukr.net>
2020-08-01 19:38:53 +0300

    Upd: add enviroment (fixes #5)

--- etc/config.js
+++ etc/config.js
@@ -4,12 +4,22 @@ dotenv.config();
 
 const appPort = process.env.appPort || 3000;
 const dbConfig = {
-    database : process.env.DB || 'base',
-    username : process.env.DBUSERNAME || 'base',
-    password : process.env.PASS || 'base',
-    host     : process.env.HOST || '127.0.0.1',
-    port     : process.env.DBPORT || 3306,
-    dialect  : process.env.DIALECT || 'mysql'
+    development : {
+        database : process.env.DB || 'base',
+        username : process.env.DBUSERNAME || 'base',
+        password : process.env.PASS || 'base',
+        host     : process.env.HOST || '127.0.0.1',
+        port     : process.env.DBPORT || 3306,
+        dialect  : process.env.DIALECT || 'mysql'
+    },
+    test : {
+        database : process.env.DB_TEST || 'testTodo',
+        username : process.env.DB_TEST_USER_NAME || 'money',
+        password : process.env.DB_TEST_PASS || 'money',
+        host     : process.env.HOST || '127.0.0.1',
+        port     : process.env.DBPORT || 3306,
+        dialect  : process.env.DIALECT || 'mysql'
+    }
 };
 
 export default {
